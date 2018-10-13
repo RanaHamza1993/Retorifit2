54dc9bfef5682bc53dd9cbc7e5a0df460777ad9c
unknown <rana.hamza2014@gmail.com>
2018-10-13 22:08:54 +0500

    Initial commit

---
+++ app/src/main/java/com/example/ranahamza/myCompany/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.ranahamza.myCompany;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Retrofit retrofit= new Retrofit.Builder().baseUrl(Api.BASE_URL)
+                                .addConverterFactory(GsonConverterFactory.create()).build();
+        Api api=retrofit.create(Api.class);
+        Call<List<Hero>> call= api.getHeroes();
+        call.enqueue(new Callback<List<Hero>>() {
+            @Override
+            public void onResponse(Call<List<Hero>> call, Response<List<Hero>> response) {
+                Toast.makeText(MainActivity.this,response.toString(),Toast.LENGTH_LONG).show();
+
+                List<Hero> heroes=response.body();
+                for(Hero h:heroes)
+                {
+                    Log.d("name",h.getName());
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Hero>> call, Throwable t) {
+                Toast.makeText(MainActivity.this,t.getMessage().toString(),Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+}
